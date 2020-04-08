086c02c60f5280008673c12384510fae099e8a0d
kevin <kroebuck529@gmail.com>
2020-04-08 14:04:46 -0400

    some GUI additions

--- graphics.py
+++ graphics.py
@@ -6,22 +6,27 @@ from grid import Grid
 class Graphics:
     """GUI for inputting sudoku puzzle to be solved."""
     def __init__(self, parent, size):
+        self.size = size
         self.values = [[0 for i in range(size)] for j in range(size)]
         self.entries = []
         self.create_grid(parent)
-        self.button = Button(parent, text="Solve", command=self.button_press)
-        self.button.grid(row=len(self.values), columnspan=len(self.values))
-        self.size = size
+        self.button_solve = Button(parent, text="Solve", command=self.button_solve_press)
+        self.button_solve.grid(row=len(self.values), columnspan=len(self.values))
 
     def create_grid(self, parent):
+        box_size = int(self.size/3)
         for i in range(len(self.values)):
             for j in range(len(self.values)):
                 temp = Entry(parent)
                 temp.grid(row=i, column=j)
-                temp.config(width=2, justify=CENTER, font="Helvetica 35 bold")
+                box_number = box_size * math.floor(i/box_size) + math.floor(j/box_size)
+                if box_number % 2:
+                    temp.config(width=2, justify=CENTER, font="Helvetica 35 bold", bg="#d9d9d9")
+                else:
+                    temp.config(width=2, justify=CENTER, font="Helvetica 35 bold")
                 self.entries.append(temp)
 
-    def button_press(self):
+    def button_solve_press(self):
         for i in range(len(self.entries)):
             if self.entries[i].get() != '':
                 k = math.floor(i / len(self.values))
@@ -45,4 +50,6 @@ class Graphics:
                 index = i * puzzle.box_size * puzzle.box_size + j
                 self.entries[index].delete(0, END)
                 self.entries[index].insert(0, puzzle.grid[i][j].known)
+                if not puzzle.grid[i][j].original:
+                    self.entries[index].config(fg="#534aff")
 
--- sudoku.py
+++ sudoku.py
@@ -7,9 +7,9 @@ from grid import Grid
 side_length = 9
 
 root = Tk()
-main = Graphics(root, side_length)
+root.title('Sudoku')
+Graphics(root, side_length)
 root.mainloop()
-init_data = main.values
 
 # initData = [[8, 0, 0, 9, 3, 0, 0, 0, 2],
 #             [0, 0, 9, 0, 0, 0, 0, 4, 0],
@@ -20,13 +20,3 @@ init_data = main.values
 #             [0, 2, 7, 0, 0, 8, 4, 0, 6],
 #             [0, 3, 0, 0, 0, 0, 5, 0, 0],
 #             [5, 0, 0, 0, 6, 2, 0, 0, 8]]
-
-# puzzle = Grid(main.values, int(side_length/3))
-#
-# if puzzle.grid_validate():
-#     sweep_count = 0
-#     while puzzle.grid_sweep():
-#         sweep_count += 1
-#
-#     puzzle.output_solution()
-#     puzzle.grid_validate()
